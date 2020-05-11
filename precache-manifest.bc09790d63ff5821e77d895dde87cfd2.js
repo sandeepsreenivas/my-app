10c3f6ffda7190305c1bb438a68e1e997affff6d
sandeep sreenivas <sandeepsreenivas@sandeeps-MacBook-Air.local>
2020-05-11 15:17:03 +0530

    Updates

---
+++ precache-manifest.bc09790d63ff5821e77d895dde87cfd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b3209fa5f7cd40855a71ee6548421ec",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5961859d106133ecd9c3",
+    "url": "/my-app/static/css/main.d3aaa3ee.chunk.css"
+  },
+  {
+    "revision": "ca96cb1d2607882b95b5",
+    "url": "/my-app/static/js/2.9a317882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.9a317882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5961859d106133ecd9c3",
+    "url": "/my-app/static/js/main.ebdcf17d.chunk.js"
+  },
+  {
+    "revision": "d21a59192a6355194f80",
+    "url": "/my-app/static/js/runtime-main.f5212b54.js"
+  }
+]);
